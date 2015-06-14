3ffd216192e6b3d71d9056709aca393f91773af4
Inferik <wotstreams.ru@gmail.com>
2015-06-14 19:21:52 +0300

    Init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace PackMods
+{
+    partial class PackMods
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PackMods));
+            this.modpack = new System.Windows.Forms.RadioButton();
+            this.clearpack = new System.Windows.Forms.RadioButton();
+            this.start_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.apply_button = new System.Windows.Forms.Button();
+            this.launcher = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // modpack
+            // 
+            this.modpack.AutoSize = true;
+            this.modpack.Location = new System.Drawing.Point(12, 15);
+            this.modpack.Name = "modpack";
+            this.modpack.Size = new System.Drawing.Size(74, 17);
+            this.modpack.TabIndex = 0;
+            this.modpack.TabStop = true;
+            this.modpack.Text = "Mod Pack";
+            this.modpack.UseVisualStyleBackColor = true;
+            // 
+            // clearpack
+            // 
+            this.clearpack.AutoSize = true;
+            this.clearpack.Location = new System.Drawing.Point(92, 15);
+            this.clearpack.Name = "clearpack";
+            this.clearpack.Size = new System.Drawing.Size(77, 17);
+            this.clearpack.TabIndex = 1;
+            this.clearpack.TabStop = true;
+            this.clearpack.Text = "Clear Pack";
+            this.clearpack.UseVisualStyleBackColor = true;
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(89, 62);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(106, 23);
+            this.start_button.TabIndex = 2;
+            this.start_button.Text = "Start Game";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Developed by Inferik";
+            // 
+            // apply_button
+            // 
+            this.apply_button.Location = new System.Drawing.Point(8, 62);
+            this.apply_button.Name = "apply_button";
+            this.apply_button.Size = new System.Drawing.Size(75, 23);
+            this.apply_button.TabIndex = 4;
+            this.apply_button.Text = "Apply";
+            this.apply_button.UseVisualStyleBackColor = true;
+            this.apply_button.Click += new System.EventHandler(this.apply_button_Click);
+            // 
+            // launcher
+            // 
+            this.launcher.AutoSize = true;
+            this.launcher.Location = new System.Drawing.Point(12, 39);
+            this.launcher.Name = "launcher";
+            this.launcher.Size = new System.Drawing.Size(144, 17);
+            this.launcher.TabIndex = 5;
+            this.launcher.Text = "Start with WOTLauncher";
+            this.launcher.UseVisualStyleBackColor = true;
+            this.launcher.CheckedChanged += new System.EventHandler(this.launcher_CheckedChanged);
+            // 
+            // PackMods
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 116);
+            this.Controls.Add(this.launcher);
+            this.Controls.Add(this.apply_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.clearpack);
+            this.Controls.Add(this.modpack);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PackMods";
+            this.Text = "PackMods";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton modpack;
+        private System.Windows.Forms.RadioButton clearpack;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button apply_button;
+        private System.Windows.Forms.CheckBox launcher;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace PackMods
+{
+    public partial class PackMods : Form
+    {
+        
+        protected string res_mods;
+        protected string currentPatch,wotPath,modPath,clearPath,currentPath;
+        protected bool isMods;
+        public PackMods()
+        {
+            wotPath = Properties.Settings.Default.wotPath;
+
+            while(!Directory.Exists(wotPath + "\\res_mods"))
+            {
+                using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+                {
+                    fbd.ShowNewFolderButton = false;
+                    fbd.Description = "Select WorldOfTanks folder (must contain res_mods folder)";
+                    DialogResult res;
+                    try
+                    {
+                        res = fbd.ShowDialog();
+                    }
+                    catch(InvalidCastException)
+                    {
+                        MessageBox.Show("Some error while selecting path");
+                        res = DialogResult.OK;
+                    }
+                    if (res == DialogResult.OK)
+                    {
+                        Properties.Settings.Default.wotPath = this.wotPath = fbd.SelectedPath;
+                        Properties.Settings.Default.Save();
+                    }
+                    else
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+            } 
+            this.res_mods = wotPath + "\\res_mods";
+
+            this.initPatch();
+            if(this.currentPatch=="")
+            {
+                MessageBox.Show("В папке res_mods не найдены папки патчей.");
+                Environment.Exit(1);
+            }
+            this.currentPath = currentPatch;
+            
+            this.isMods = !Directory.Exists(currentPath + "_m");
+            modPath = currentPath + (this.isMods ? "" : "_m");
+            clearPath = currentPath + (this.isMods ? "_c" : "");
+            if (!Directory.Exists(this.isMods ? clearPath : modPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(this.isMods ? clearPath : modPath);
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to create directory: " + (this.isMods ? clearPath : modPath));
+                    Environment.Exit(2);
+                }
+            }
+            InitializeComponent();
+        }
+        private void initPatch()
+        {
+            string[] patches = Directory.GetDirectories(this.res_mods);
+            Array.Sort(patches);
+            int currentPatch = patches.Length - 1;
+            string pattern = @"([0-9])(\.[0-9]+){2,}";
+            Regex regPatch = new Regex(pattern);
+            string patch = "";
+            foreach(string path in patches)
+            {
+                MatchCollection matches = regPatch.Matches(path);
+                if(matches.Count>0)
+                {
+                    if (res_mods + "\\" + matches[0].Value != path)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        patch = path;
+                    }
+                }
+            }
+            this.currentPatch = patch;
+        }
+
+        private void changeMode()
+        {
+            bool isMods = modpack.Checked;
+            if (this.isMods == isMods)
+                return;
+            else this.isMods = isMods;
+
+            try
+            {
+                Directory.Move(currentPath, currentPath + (this.isMods ? "_c" : "_m"));
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Ошибка презаписи каталогов [1]: " + ex.Message);
+            }
+            try
+            {
+                Directory.Move(currentPath + (!this.isMods ? "_c" : "_m"), currentPath);
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Ошибка презаписи каталогов [2]: " + ex.Message);
+            }
+
+            return;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            (this.isMods ? modpack : clearpack).Checked = true;
+            launcher.Checked = Properties.Settings.Default.launcher;
+
+        }
+
+        private void apply_button_Click(object sender, EventArgs e)
+        {
+            this.changeMode();
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            this.changeMode();
+            Process.Start(wotPath + "\\" + (launcher.Checked ? "WoTLauncher.exe" : "WorldOfTanks.exe"));
+        }
+
+        private void launcher_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.launcher = launcher.Checked;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
